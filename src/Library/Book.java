931080d064378ad73bcc2ebbe61d3aa703d54dc3
Javier <d2j5@hotmail.com>
2023-05-22 22:56:30 -0400

    commit

--- src/Library/Book.java
+++ src/Library/Book.java
@@ -1,5 +1,85 @@
 package Library;
-
+import java.time.LocalDate;
 public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+    private int pages;
+    private String category;
+    private boolean isOnLoan;
+    private LocalDate loanDueDate;
+
+    public Book(String title, String author, int publicationYear, int pages, String category) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.pages = pages;
+        this.category = category;
+        this.isOnLoan = false;
+        this.loanDueDate = null;
+    }
+
+    // Getters and setters
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public boolean isOnLoan() {
+        return isOnLoan;
+    }
+
+    public void setOnLoan(boolean onLoan) {
+        isOnLoan = onLoan;
+    }
+
+    public LocalDate getLoanDueDate() {
+        return loanDueDate;
+    }
+
+    public void setLoanDueDate(LocalDate loanDueDate) {
+        this.loanDueDate = loanDueDate;
+    }
+    @Override
+    public String toString() {
+        return "Title: " + title + ", Author: " + author + ", Publication Year: " + publicationYear + ", Pages: " + pages + ", Category: " + category;
+    }
 
 }
--- src/Library/User.java
+++ src/Library/User.java
@@ -1,6 +1,51 @@
 package Library;
-
+import java.util.ArrayList;
+import java.util.List;
 public class User {
+    private String name;
+    private int libraryCardNumber;
+    private List<Book> booksOnLoan;
 
+    public User(String name, int libraryCardNumber) {
+        this.name = name;
+        this.libraryCardNumber = libraryCardNumber;
+        this.booksOnLoan = new ArrayList<>();
+    }
+
+    // Getters and setters
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLibraryCardNumber() {
+        return libraryCardNumber;
+    }
+
+    public void setLibraryCardNumber(int libraryCardNumber) {
+        this.libraryCardNumber = libraryCardNumber;
+    }
+
+    public void setBooksOnLoan(List<Book> booksOnLoan) {
+        this.booksOnLoan = booksOnLoan;
+    }
+
+    public List<Book> getBooksOnLoan() {
+        return booksOnLoan;
+    }
+
+    // Add a book to the user's list of books on loan
+    public void addBookOnLoan(Book book) {
+        booksOnLoan.add(book);
+    }
+
+    // Remove a book from the user's list of books on loan
+    public void removeBookOnLoan(Book book) {
+        booksOnLoan.remove(book);
+    }
 
 }
